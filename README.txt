161ab9c2a221f3bdf0d1573fc5255ccccf9e9495
Addi <coxonmarc1@gmail.com>
2020-07-14 15:35:23 +0700

    halfway there

--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
 TO DO LIST
+Required Features 🎯
+
+    [ ] User can add a todo.
+    [ ] User can remove a todo.
+    [ ] User can mark a todo done.
+    [ ] User can see a list of their todos.
---
+++ js/main.js
@@ -0,0 +1,76 @@
+// "To Do" script for website
+
+// Declare array variable
+let todoList = [];
+
+// add listener for filter checkbox
+
+let checkbox = document.querySelector("input[name=filter]");
+
+// ToDo function
+
+const addItem = () => {
+
+    // recieve input from HTML
+    let todo = document.getElementById("itemInput").value
+
+    // declare object containing item + bool check complete or not
+    let itemTodo = { contents: todo, complete: false }
+
+    // add input item to object "itemTodo"
+    todoList.push(itemTodo);
+    console.log(todoList);
+    render();
+
+}
+
+// mark each item done or not done
+const toggleDone = (index) => {
+
+    if (todoList[index].complete === true) {
+        todoList[index].complete = false;
+    } else {
+        todoList[index].complete = true;
+
+    }
+    render();
+}
+
+// remove an item from the list 
+const remove = (index) => {
+    todoList.splice(index, 1);
+    render();
+}
+
+// true or false check for checkbox
+checkbox.addEventListener('change', function() {
+    let filterCheck = false;
+    if (this.checked) {
+        filterCheck = true
+        filter();
+    } else {
+        return;
+    }
+    return;
+})
+
+const filter = () => {
+    let filterList = todoList.filter((item) => item.complete === true)
+    console.log(filterList)
+
+    document.getElementById("listItemArea").innerHTML = filterList;
+
+}
+
+const render = () => {
+    let todoHTML = todoList.map((item, index) => {
+
+        if (item.complete == false) {
+            return `<li class="list">TODO:${item.contents} <a onclick="toggleDone(${index})" href="#">Mark Done</a><a href="#" onclick="remove(${index})"> x</a></li>`;
+        } else {
+            return `<li class="list"><s>TODO:${item.contents}</s> <a onclick="toggleDone(${index})" href="#"><s>Mark Not Done</s></a><a href="#" onclick="remove(${index})"><s>x</s></a></li>`;
+        }
+    }).join('');
+
+    document.getElementById("listItemArea").innerHTML = todoHTML;
+}
